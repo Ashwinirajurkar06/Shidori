f7abd724e380ccfc736b43a34d128f2a305e99e3
Ashwini Rajurkar <ashwinirajku@gmail.com>
2025-12-23 17:39:59 +0530

    project initialization

---
+++ src/main/java/com/shidori/ShidoriWebApplication.java
@@ -0,0 +1,13 @@
+package com.shidori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShidoriWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShidoriWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shidori/ShidoriWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shidori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShidoriWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
